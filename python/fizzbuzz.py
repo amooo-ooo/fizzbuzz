8df936abcb2bb12851453b00da47612e303cf816
amoooooo <83256424+amooo-ooo@users.noreply.github.com>
2023-09-28 20:06:40 +1300

    Update fizzbuzz.py

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -6,8 +6,11 @@ def fizzbuzz(define: dict, reps: range) -> Generator[int | str, None, None]:
         e = tuple(a for a, b in define if i % b == 0)
         yield i if e == () else ''.join(e)
 
-if __name__ == '__main__':
+def main():
     context = {'Fizz': 3, 'Buzz': 5}
     for i in fizzbuzz(context, range(100)):
         print(i)
+
+if __name__ == '__main__':
+    main()
 
